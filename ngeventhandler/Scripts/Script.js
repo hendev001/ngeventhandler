2f493cbd9947b8c978de80568b77c74a99861f2d
Samuel Henga <samuelhen@gmail.com>
2016-09-29 13:48:21 -0500

    Angular simple site

---
+++ ngeventhandler/Scripts/Script.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular
+            .module("myModule", [])
+             .controller("myController", function ($scope) {
+
+
+                 var technologies = [
+
+
+                     { name: "C#", likes: 0, dislikes: 20 },
+                       { name: "Java", likes: 0, dislikes: 20 },
+                         { name: "SQL", likes: 0, dislikes: 20 },
+                           { name: "C++", likes: 0, dislikes: 20 },
+                             { name: "ASP.NET", likes: 0, dislikes: 20 },
+
+                 ];
+
+                 $scope.technologies = technologies;
+
+                 $scope.incrementlikes = function (technology) {
+                     technology.likes++;
+                 }
+             
+                 $scope.decrementdislikes = function (technology) {
+                     technology.dislikes--;
+                 }
+             });
